68ad6cf641b90a6879fbc81e4c60a2f9ce9bc930
RoMANzhula <roman1985al@ukr.net>
2024-11-20 17:31:22 +0200

    Add method clone()

--- src/clone_method/Main.java
+++ src/clone_method/Main.java
@@ -1,2 +1,14 @@
-package clone_method;public class Main {
+package clone_method;
+
+public class Main {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        MyObject object1 = new MyObject("Bob", 20);
+        MyObject object2 = (MyObject) object1.clone();
+
+        System.out.println("Object #1 is: " + object1);                         // output: MyObject{name=Bob, age=20}
+
+        System.out.println("Object #2 is a clone of Object #1: " + object2);    // output: MyObject{name=Bob, age=20}
+    }
+
 }
--- src/clone_method/MyObject.java
+++ src/clone_method/MyObject.java
@@ -1,2 +1,23 @@
-package clone_method;public class MyObject {
+package clone_method;
+
+// Create class that implements interface Cloneable
+public class MyObject implements Cloneable {
+
+    String name;
+    int age;
+
+    public MyObject(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "MyObject{name=" + name + ", age=" + age + "}";
+    }
 }
